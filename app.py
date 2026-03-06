9eed0745dcbc86db427b6d9f5ab445f287834dd0
vikysoyeu <haidaonguyen2020@gmail.com>
2026-03-06 11:06:05 +0700

    v2 implement uniform interface REST API

--- app.py
+++ app.py
@@ -1,22 +1,49 @@
-from flask import Flask, jsonify
-from database import get_all_users, get_user
+from flask import Flask, jsonify, request
+import database
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "REST API demo"
+# GET all users
+@app.route("/users", methods=["GET"])
+def get_users():
+    return jsonify(database.get_all_users())
 
-# Uniform Interface
-@app.route("/users")
-def users():
-    return jsonify(get_all_users())
 
-@app.route("/users/<int:user_id>")
-def user(user_id):
-    u = get_user(user_id)
-    if u:
-        return jsonify(u)
+# GET one user
+@app.route("/users/<int:user_id>", methods=["GET"])
+def get_user(user_id):
+    user = database.get_user(user_id)
+
+    if user:
+        return jsonify(user)
+
+    return {"error": "User not found"}, 404
+
+
+# POST create user
+@app.route("/users", methods=["POST"])
+def create_user():
+    data = request.json
+
+    new_user = {
+        "id": len(database.users) + 1,
+        "name": data["name"]
+    }
+
+    database.users.append(new_user)
+
+    return jsonify(new_user)
+
+
+# DELETE user
+@app.route("/users/<int:user_id>", methods=["DELETE"])
+def delete_user(user_id):
+
+    for u in database.users:
+        if u["id"] == user_id:
+            database.users.remove(u)
+            return {"message": "deleted"}
+
     return {"error": "User not found"}, 404
 
 
